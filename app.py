5ea529c363ce261ba9060568ddb8edc3908b7a74
byldiga52 <nextgenri4@gmail.com>
2024-05-25 16:27:15 +0300

    Ваше сообщение о коммите

--- app.py
+++ app.py
@@ -13,30 +13,61 @@ def index():
     # Передаем данные в HTML шаблон
     return render_template('tcard.html', anime_data=data)
 
+
+
 @app.route('/load_more_anime')
 def load_more_anime():
-    page = request.args.get('page', type=int)
-    per_page = request.args.get('per_page', type=int)
-    genres_param = request.args.get('genres', '')
+    page = int(request.args.get('page', 0))
+    per_page = int(request.args.get('per_page', 10))
+    start = page * per_page
+    end = start + per_page
+    try:
+        with open('anime_final.json', 'r', encoding='utf-8') as file:
+            data = json.load(file)
+        # Добавляем уникальные идентификаторы к объектам аниме
+        for idx, anime in enumerate(data):
+            anime['id'] = idx + 1
+        return jsonify(anime_data=data[start:end])
+    except Exception as e:
+        return jsonify(error=str(e))
 
-    # Преобразование параметра жанров в список
-    selected_genres = genres_param.split(',') if genres_param else []
+@app.route('/anime/<int:anime_id>')
+def anime_detail(anime_id):
+    with open('anime_final.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)
+    
+    # Добавляем уникальные идентификаторы к объектам аниме
+    for idx, anime in enumerate(data):
+        anime['id'] = idx + 1
 
-    # Открыть файл и загрузить все данные
+    anime = next((item for item in data if item['id'] == anime_id), None)
+    if anime:
+        return render_template('anime_detail.html', anime=anime)
+    else:
+        return "Anime not found", 404
+
+
+
+@app.route('/search')
+def search():
+    search_text = request.args.get('text', '')
+
+    # Открываем файл с данными аниме
     with open('anime_final.json', 'r', encoding='utf-8') as file:
         all_data = json.load(file)
 
-    # Фильтрация данных по всем выбранным жанрам
-    filtered_data = all_data
-    for genre in selected_genres:
-        filtered_data = [anime for anime in filtered_data if genre in anime.get('Tags', [])]
+    # Нормализуем текст поиска
+    normalized_search_text = search_text.lower()
 
-    # Вычислить индексы данных для текущей страницы
-    start_index = page * per_page
-    end_index = start_index + per_page
+    # Фильтруем данные по введенному тексту поиска
+    search_results = [anime for anime in all_data 
+                      if normalized_search_text in anime['Short title'].lower()
+                      or normalized_search_text in anime['Title'].lower()]
 
-    # Отправить часть отфильтрованных данных на клиент
-    return jsonify(anime_data=filtered_data[start_index:end_index])
+    # Возвращаем результаты поиска в формате JSON
+    return jsonify(search_results=search_results)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/main.js
+++ static/main.js
@@ -2,15 +2,17 @@ const btn = document.getElementById('genre-btn');
 const dropdown = document.getElementById('drop');
 let selectedGenres = [];
 
-btn.addEventListener('click', () => {
-    if (dropdown.classList.contains('hide')) {
-        dropdown.classList.remove('hide');
-        dropdown.classList.toggle('showb');
-    } else if (dropdown.classList.contains('showb')) {
-        dropdown.classList.remove('showb');
-        dropdown.classList.toggle('hide');
-    }
-});
+if (btn && dropdown) {
+    btn.addEventListener('click', () => {
+        if (dropdown.classList.contains('hide')) {
+            dropdown.classList.remove('hide');
+            dropdown.classList.toggle('showb');
+        } else if (dropdown.classList.contains('showb')) {
+            dropdown.classList.remove('showb');
+            dropdown.classList.toggle('hide');
+        }
+    });
+}
 
 let anims = document.getElementsByClassName("anims");
 Array.from(anims).forEach(function (anim) {
@@ -60,7 +62,8 @@ function updateAnimeList() {
         if (xhr.readyState === XMLHttpRequest.DONE) {
             if (xhr.status === 200) {
                 const data = JSON.parse(xhr.responseText);
-                appendAnimeToPage(data.anime_data);
+                console.log('Received anime data:', data); // Выводим данные аниме в консоль
+                appendAnimeToPage(data.anime_data); // Обновляем список аниме на странице
             } else {
                 console.error('Error loading anime data:', xhr.status);
             }
@@ -84,29 +87,101 @@ function updateAnimeList() {
 // Функция для обновления размера выпадающего меню жанров при изменении размера окна
 const dropdownBtn = document.querySelector('.genre-btn');
 const dropdownMenu = document.querySelector('.dropdown');
-dropdownMenu.style.width = dropdownBtn.offsetWidth - 23 + 'px';
+if (dropdownBtn && dropdownMenu) {
+    dropdownMenu.style.width = dropdownBtn.offsetWidth - 23 + 'px';
 
-window.addEventListener('resize', () => {
-    dropdownMenu.style.width = dropdownBtn.offsetWidth - 20 + 'px';
-});
+    window.addEventListener('resize', () => {
+        dropdownMenu.style.width = dropdownBtn.offsetWidth - 20 + 'px';
+    });
+}
 
 // Пагинация
 let currentPage = 1;
 
-document.getElementById('prev-page').addEventListener('click', () => {
-    if (currentPage > 1) {
-        currentPage--;
+const prevPageBtn = document.getElementById('prev-page');
+const nextPageBtn = document.getElementById('next-page');
+
+if (prevPageBtn && nextPageBtn) {
+    prevPageBtn.addEventListener('click', () => {
+        if (currentPage > 1) {
+            currentPage--;
+            updatePageNumber(); // Сначала обновляем номер страницы
+            updateAnimeList(); // Затем загружаем новые данные
+        }
+    });
+
+    nextPageBtn.addEventListener('click', () => {
+        currentPage++;
         updatePageNumber(); // Сначала обновляем номер страницы
         updateAnimeList(); // Затем загружаем новые данные
-    }
-});
-
-document.getElementById('next-page').addEventListener('click', () => {
-    currentPage++;
-    updatePageNumber(); // Сначала обновляем номер страницы
-    updateAnimeList(); // Затем загружаем новые данные
-});
+    });
+}
 
 function updatePageNumber() {
-    document.getElementById('page-number').textContent = currentPage;
+    const pageNumber = document.getElementById('page-number');
+    if (pageNumber) {
+        pageNumber.textContent = currentPage;
+    }
+}
+
+// Функция для обновления интерфейса с результатами поиска
+function updateSearchResults(results) {
+    const searchResults = document.getElementById('search-results');
+    // Очищаем контейнер для результатов перед обновлением
+    if (searchResults) {
+        searchResults.innerHTML = '';
+
+        // Проверяем, есть ли результаты поиска
+        if (results && results.length > 0) {
+            // Показываем контейнер с результатами поиска
+            searchResults.style.display = 'block';
+
+            // Создаем элементы списка для каждого результата поиска
+            results.forEach(result => {
+                const listItem = document.createElement('li');
+                // Предположим, что название аниме может находиться в свойстве 'Title' или 'Short Title'
+                const animeTitle = result['Title'] || result['Short title'];
+                listItem.textContent = animeTitle;
+                // Добавляем обработчик события клика на элемент списка для выбора аниме
+                listItem.addEventListener('click', () => {
+                    document.getElementById('search-input').value = animeTitle; // Заполнение поля поиска выбранным аниме
+                    searchResults.style.display = 'none'; // Скрытие выпадающего списка
+                });
+                searchResults.appendChild(listItem);
+            });
+        } else {
+            // Скрываем контейнер, если результаты поиска отсутствуют
+            searchResults.style.display = 'none';
+        }
+    }
+}
+
+// Обработчик ввода текста в поле поиска
+const searchInput = document.getElementById('search-input');
+if (searchInput) {
+    searchInput.addEventListener('input', () => {
+        const searchText = searchInput.value;
+        if (searchText.trim() !== '') {
+            // Отправляем запрос на сервер для поиска аниме по введенному тексту
+            const xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function() {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status === 200) {
+                        const data = JSON.parse(xhr.responseText);
+                        updateSearchResults(data.search_results); // Обновляем результаты поиска
+                    } else {
+                        console.error('Error searching anime:', xhr.status);
+                    }
+                }
+            };
+            xhr.open('GET', `/search?text=${encodeURIComponent(searchText)}`, true);
+            xhr.send();
+        } else {
+            // Если поле поиска пустое, скрываем выпадающее меню
+            const searchResults = document.getElementById('search-results');
+            if (searchResults) {
+                searchResults.style.display = 'none';
+            }
+        }
+    });
 }
